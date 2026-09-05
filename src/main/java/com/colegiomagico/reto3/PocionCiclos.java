339be4eeea150ba0518f6e6025f36deeeb0b5be8
giraldoalzatehaslysofia-hash <giraldoalzatehaslysofia@gmail.com>
2026-09-05 16:45:09 +0000

    feat: Se implementa un ciclo for que al recibir una cantidad imprime la palabra pocion las veces que se le indique

--- src/main/java/com/colegiomagico/reto3/PocionCiclos.java
+++ src/main/java/com/colegiomagico/reto3/PocionCiclos.java
@@ -6,7 +6,11 @@ package com.colegiomagico.reto3;
 public class PocionCiclos {
 
     public static String preparar(int cantidad) {
-        return "pocion"; // TODO: Cambia esto
+        String palabra = "";
+        for(int i = 0; i<cantidad; i++){
+            palabra += "Pocion";
+        }
+        return palabra; // TODO: Cambia esto
     }
 
     public static void main(String[] args) {
