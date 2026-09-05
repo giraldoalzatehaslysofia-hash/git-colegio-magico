e4d723e8cbb073759137959b1bcd5803e0cb1b3d
giraldoalzatehaslysofia-hash <giraldoalzatehaslysofia@gmail.com>
2026-09-05 15:58:20 +0000

    feat: Se agrega la implementacion de la funcion llamada invocar

--- src/main/java/com/colegiomagico/reto1/HechizoIniciacion.java
+++ src/main/java/com/colegiomagico/reto1/HechizoIniciacion.java
@@ -6,7 +6,7 @@ package com.colegiomagico.reto1;
 public class HechizoIniciacion {
 
     public static String invocar() {
-        return ""; // TODO: Cambia esto
+        return "Mi nombre es hasly y mi profesor se llama juan jose"; // TODO: Cambia esto
     }
 
     public static void main(String[] args) {
