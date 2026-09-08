47f242ee9403dce7e3f63489affb3f74a6ebc132
giraldoalzatehaslysofia-hash <giraldoalzatehaslysofia@gmail.com>
2026-09-08 01:53:33 +0000

    feat:Se creo la variable filtrados y se corrigio la posicion del return :3

--- src/main/java/com/colegiomagico/reto8/InventarioPociones.java
+++ src/main/java/com/colegiomagico/reto8/InventarioPociones.java
@@ -6,16 +6,15 @@ package com.colegiomagico.reto8;
 public class InventarioPociones {
 
     public static java.util.List<String> filtrar(java.util.List<String> ingredientes) {
-        return new java.util.ArrayList<>(); 
+    java.util.List<String> filtrados = new java.util.ArrayList<>();
 
-            for (String ingrediente : ingredientes) {
-            if (ingrediente.startsWith("A")) {
-                filtrados.add(ingrediente);
-            }
+    for (String ingrediente : ingredientes) {
+        if (ingrediente.startsWith("A")) {
+            filtrados.add(ingrediente);
         }
-        return filtrados;
     }
-    }
+    return filtrados;
+}
 
     public static void main(String[] args) {
         java.util.List<String> lista = java.util.Arrays.asList("Ajenjo", "Miel");
